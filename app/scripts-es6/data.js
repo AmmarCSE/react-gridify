8512b0af8ddc4ab2d73d368e97f7669ae085b3b1
AmmarCSE <ammar.abu-yasein@farsightsoftware.com>
2016-05-11 16:29:33 +0300

    gulp serve task

--- app/scripts-es6/data.js
+++ app/scripts-es6/data.js
@@ -1,3 +1,4 @@
+
 var data = [
   {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
   {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,7 @@ import gulpLoadPlugins from 'gulp-load-plugins';
 import browserSync from 'browser-sync';
 
 const $ = gulpLoadPlugins();
+const reload = browserSync.reload;
 
 gulp.task('scripts', () => {
   return gulp.src('app/scripts-es6/*.js')
@@ -10,6 +11,26 @@ gulp.task('scripts', () => {
     .pipe($.babel({presets: ["es2015", "react"]}))
     .pipe($.sourcemaps.write())
     .pipe(gulp.dest('app/scripts'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('serve', [ 'scripts'], () => {
+  browserSync({
+    notify: false,
+    port: 9000,
+    server: {
+      baseDir: ['app'],
+      routes: {
+        '/bower_components': 'bower_components'
+      }
+    }
+  });
+
+  gulp.watch([
+    'app/*.html'
+  ]).on('change', reload);
+
+  gulp.watch('app/scripts-es6/*.js', ['scripts']);
 });
 
 gulp.task('default', () => {
